043b4e14aba00d4f404cf86156394545a13ad87c
Tim Ekl <lithium3141@gmail.com>
2011-06-28 18:40:52 -0600

    Test parse(), constructor

--- src/main/java/com/nijiko/test/MessageHelperTest.java
+++ src/main/java/com/nijiko/test/MessageHelperTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import junit.framework.Assert;
 
+import org.bukkit.Server;
 import org.junit.Test;
 
 import com.nijiko.MessageHelper;
@@ -32,15 +33,72 @@ public class MessageHelperTest {
 		add("yellow");
 		add("white");
 	}};
+	
+	protected class MockCommandSender implements org.bukkit.command.CommandSender {
+		
+		// List of messages received, in chronological order
+		protected List<String> sentMessages;
+		
+		MockCommandSender() {
+			this.sentMessages = new ArrayList<String>();
+		}
 
-	@Test
-	public void testMessageHelper() {
-		fail("Not yet implemented");
+		@Override
+		public Server getServer() {
+			return null;
+		}
+
+		@Override
+		public boolean isOp() {
+			return false;
+		}
+
+		@Override
+		public void sendMessage(String message) {
+			this.sentMessages.add(message);
+		}
+		
 	}
 
 	@Test
-	public void testParse() {
-		fail("Not yet implemented");
+	public void testMessageHelper() {
+		MessageHelper target = new MessageHelper(null);
+		Assert.assertNull("MessageHelper did not accept null CommandSender", target.sender);
+		
+		MockCommandSender mockSender = new MockCommandSender();
+		target = new MessageHelper(mockSender);
+		Assert.assertEquals("MessageHelper did not store mock CommandSender", mockSender, target.sender);
+	}
+
+	@Test
+	public void testParseNoReplace() {
+		Assert.assertEquals("This string has no colors!", MessageHelper.parse("This string has no colors!"));
+		Assert.assertEquals("This one doesn't either!", MessageHelper.parse("This one doesn't either!"));
+		Assert.assertEquals("Here are some color names: blue red yellow green", MessageHelper.parse("Here are some color names: blue red yellow green"));
+		Assert.assertEquals("And some special characters: &x &z &? &*", MessageHelper.parse("And some special characters: &x &z &? &*"));
+	}
+	
+	@Test
+	public void testParseSingleReplace() {
+		for(int i = 0; i < this.recognizedColors.size(); i++) {
+			Assert.assertEquals("\u00A7" + Integer.toHexString(i), MessageHelper.parse("&" + Integer.toHexString(i)));
+		}
+	}
+	
+	@Test
+	public void testParseInlineReplace() {
+		for(int i = 0; i < this.recognizedColors.size(); i++) {
+			String startString = "This is a color: &" + Integer.toHexString(i) + ". Isn't it nice?";
+			Assert.assertEquals("This is a color: \u00A7" + Integer.toHexString(i) + ". Isn't it nice?", MessageHelper.parse(startString));
+		}
+	}
+	
+	@Test
+	public void testParseMultipleReplace() {
+		for(int i = 1; i < this.recognizedColors.size(); i++) {
+			String startString = "Multiple colors! &" + Integer.toHexString(i - 1) + " &" + Integer.toHexString(i);
+			Assert.assertEquals("Multiple colors! \u00A7" + Integer.toHexString(i - 1) + " \u00A7" + Integer.toHexString(i), MessageHelper.parse(startString));
+		}
 	}
 	
 	@Test
@@ -67,7 +125,7 @@ public class MessageHelperTest {
 	}
 	
 	@Test
-	public void testMultipleReplace() {
+	public void testColorizeMultipleReplace() {
 		for(int i = 1; i < this.recognizedColors.size(); i++) {
 			String startString = "Multiple colors! <" + this.recognizedColors.get(i - 1) + "> <" + this.recognizedColors.get(i) + ">";
 			Assert.assertEquals("Multiple colors! \u00A7" + Integer.toHexString(i - 1) + " \u00A7" + Integer.toHexString(i), MessageHelper.colorize(startString));
